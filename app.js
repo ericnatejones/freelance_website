e59adddb5251420131b0dcf191a71f7ca35e4e73
Eric Jonese <ericnatejones@gmail.com>
2017-04-08 17:37:35 -0600

    page switchstuff

---
+++ app.js
@@ -0,0 +1,27 @@
+function switchPage(whichPage){
+  $(".page").hide();
+  $(whichPage).show();
+}
+switchPage("#home-page-content")
+
+$(".switch-to-home").click(function(){
+  switchPage("#home-page-content")
+})
+$(".switch-to-page-1").click(function(){
+  switchPage("#info-page-1-content")
+})
+$(".switch-to-page-2").click(function(){
+  switchPage("#info-page-2-content")
+})
+$(".switch-to-page-3").click(function(){
+  switchPage("#info-page-3-content")
+})
+$(".switch-to-page-4").click(function(){
+  switchPage("#info-page-4-content")
+})
+$(".switch-to-page-5").click(function(){
+  switchPage("#info-page-5-content")
+})
+$(".switch-to-page-6").click(function(){
+  switchPage("#info-page-6-content")
+})
